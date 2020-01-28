5f33f72ad4ec2c6eaed8cda4d4ab6497cbff0e59
erwannbroye <erwann.broye-hombert@epitech.eu>
2020-01-28 19:07:23 +0100

    thomas je te baise

--- Assets/scripts/player/characterController.cs
+++ Assets/scripts/player/characterController.cs
@@ -32,6 +32,18 @@ public class characterController : MonoBehaviour
     public Camera cam;
     Animator anim;
     public bool CameraNormalView;
+    bool crouch = false;
+    RaycastHit terrainHit;
+    float fallTime = 0;
+    float distanceCovered = 0;
+
+    //audio
+    public AudioSource audioSource;
+
+    public AudioClip[] stoneClips;
+    public AudioClip[] woodClips;
+    public AudioClip[] dirtClips;
+    AudioClip previousClip;
 
     void Start()
     {
@@ -49,40 +61,36 @@ public class characterController : MonoBehaviour
          move.y = 0;
     }
 
-    public float getXAxis()
-    {
-        return(xAxis);
-    }
     void LateUpdate()
     {
         //control
         xAxis = (Input.GetAxis("Vertical"));
         zAxis = (Input.GetAxis("Horizontal"));
-
-		if (Cursor.lockState == CursorLockMode.Locked)
-		{
-			run();
-			jump();
-			interact();
-		}
-
-        if (Controller.isGrounded && Climbing == true && (xAxis < 0 || zAxis != 0))
-            Climbing = false;
-
-        //end control
-
-        // apply move
-
-        if (Climbing) {
-            move.y = xAxis * Speed;
-            xAxis = 0;
-            zAxis = 0;
-        }
-        float yStore = move.y;
-
+        run();
+        jump();
+        interact();
+        crouching();
         AimingMove();
+        playFallingSound();
+
         
-        move.y = yStore;
+        // if (Controller.isGrounded && Climbing == true && (xAxis < 0 || zAxis != 0))
+        //     Climbing = false;
+        // if (Climbing) {
+        //     move.y = xAxis * Speed;
+        //     xAxis = 0;
+        //     zAxis = 0;
+        // }
+        // if (Controller.isGrounded)
+        if (Controller.velocity.x != 0 || Controller.velocity.z != 0) {
+            distanceCovered += Controller.velocity.magnitude * Time.deltaTime;
+            if (distanceCovered > 3) {
+                footstepsSound();
+                distanceCovered = 0;  
+            }
+        }
+        
+        
 
         // character slide down slopes 
         // if (!onSlop) {
@@ -94,36 +102,67 @@ public class characterController : MonoBehaviour
             move.y = move.y + ((Physics.gravity.y - playerWeight) * Time.deltaTime);
 
         Controller.Move(move * Time.deltaTime);
-
         onSlop = (Vector3.Angle (Vector3.up, hitNormal) <= Controller.slopeLimit);
     }
 
-    void BasicMove()
+    AudioClip getClip(AudioClip[] clipArray)
     {
-        var camera = Camera.main;
-        Vector3 forward = cam.transform.forward;
-        Vector3 right = cam.transform.right;
- 
-        forward.y = 0f;
-        right.y = 0f;
- 
-        forward.Normalize ();
-        right.Normalize ();
- 
-        move = forward * xAxis + right * zAxis;
+        AudioClip selectedClip = clipArray[Random.Range(0, clipArray.Length - 1)];
+        while (selectedClip == previousClip ) {
+            selectedClip = clipArray[Random.Range(0, clipArray.Length - 1)];
+        }
+        previousClip = selectedClip;
+        return selectedClip;
+    }
 
-        if (xAxis >= 0.5 && zAxis >= 0.5)
-            move = move.normalized * Speed;
-        else
-            move = move * Speed;
+    void footstepsSound()
+    {
+        Physics.Raycast(transform.position +  (Vector3.down), Vector3.down, out terrainHit, Controller.bounds.extents.y + 0.5f);
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 4, Color.red);
+        audioSource.pitch = Random.Range(0.9f, 1.1f);
+        audioSource.volume = Random.Range(0.8f, 1f);
+        if (terrainHit.collider != null) {
+            Debug.Log(terrainHit.collider.tag);
+            if (terrainHit.collider.tag == "terrain")
+                audioSource.PlayOneShot(getClip(stoneClips), 1f);
+            else
+                audioSource.PlayOneShot(getClip(dirtClips), 1f);
+        }
+    }
 
-        if ((xAxis != 0 || zAxis != 0))
-                transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.LookRotation (move), desiredRotationSpeed);
+    void playFallingSound()
+    {
+        if (Controller.isGrounded == false)
+            fallTime += Time.deltaTime;
+        else {
+            if (fallTime >= 0.25f) {
+                footstepsSound();
+                fallTime = 0;
+            }
+        }
+    }
+
+    void crouching()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+            Controller.height = 2.5f;
+            crouch = true;
+            Speed = 2;
+        }
+        if ( Input.GetButtonUp("Fire1")) {
+            crouch = false;
+            Speed = 5;
+        }
+        if (!Physics.Raycast(transform.position + Vector3.up,  Vector3.up,2f) && crouch == false && Controller.height < 4)
+            Controller.height += (10 * Time.deltaTime);
+
+
     }
 
     void AimingMove()
     {
         
+        float yStore = move.y;
         move = (transform.forward * xAxis) + (transform.right * zAxis);
 
         // slow down diagonal move
@@ -131,6 +170,7 @@ public class characterController : MonoBehaviour
             move = move.normalized * Speed;
         else
             move = move * Speed;
+        move.y = yStore;
     }
 
     void jump() 
@@ -143,11 +183,10 @@ public class characterController : MonoBehaviour
 
     void run() 
     {
-        if (Input.GetButtonDown("Fire3") && xAxis > 0)
-            Speed = runSpeed
- * 1.5f;
+        if (Input.GetButtonDown("Fire3") && xAxis > 0 && crouch == false)
+            Speed = runSpeed;
         if (Input.GetButtonUp("Fire3"))
-            Speed = runSpeed
+            Speed = runSpeed / 2;
 ;
     }
 
